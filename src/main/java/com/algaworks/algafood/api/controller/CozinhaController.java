d535ad90f33ec02da28ba283462c42bfce5faded
Ricardo Lima Oliveira <ricardolima.ti@gmai.com>
2020-03-02 23:10:23 -0300

    4.25

--- src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -5,13 +5,16 @@ package com.algaworks.algafood.api.controller;
 
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -49,11 +52,16 @@ public class CozinhaController {
 
 		Cozinha cozinha = cozinhaRepository.porId(cozinhaId);
 		
+		if(cozinha != null) {
+			return ResponseEntity.ok(cozinha);
+		}
+		
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+		
 //		return cozinhaRepository.porId(cozinhaId);
 //		return ResponseEntity.status(HttpStatus.OK).body(cozinha);
 //		return ResponseEntity.ok(cozinha);
 		
-		//teste branch
 		
 //		HttpHeaders headers = new HttpHeaders();
 //		headers.add(HttpHeaders.LOCATION, "http://localhost:8080/cozinhas");
@@ -62,12 +70,32 @@ public class CozinhaController {
 //				.status(HttpStatus.FOUND)
 //				.headers(headers)
 //				.build();
-		if(cozinha != null) {
-			return ResponseEntity.ok(cozinha);
-		}
-		
-		return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
 //		return ResponseEntity.notFound().build();
 	}
 	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Cozinha adicionar(@RequestBody Cozinha cozinha) {
+		return cozinhaRepository.salvar(cozinha);
+	}
+	
+	@PutMapping("/{cozinhaId}")
+	public ResponseEntity<Cozinha> alterar(@PathVariable Long cozinhaId,
+			@RequestBody Cozinha cozinha) {
+		
+		Cozinha cozinhaAtual = cozinhaRepository.porId(cozinhaId);
+		
+//		cozinha.setNome(cozinhaAtual.getNome());
+		
+		BeanUtils.copyProperties(cozinha, cozinhaAtual, "id");
+		
+		if(cozinhaAtual != null) {
+			
+			cozinhaAtual = cozinhaRepository.salvar(cozinhaAtual);
+			
+			return ResponseEntity.ok(cozinhaAtual);
+		}
+		
+		return ResponseEntity.notFound().build();
+	}
 }
